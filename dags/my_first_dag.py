f52934f430c15b348adb1ea3dd05a7b8c8b93d2e
Belkacem Lahouel <belkacem.lahouel@gmail.com>
2025-11-26 15:55:07 +0100

    Add variable update code

--- dags/my_first_dag.py
+++ dags/my_first_dag.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+from airflow.models import Variable
 from datetime import datetime, timedelta
 
 # Paramètres par défaut du DAG
@@ -19,6 +20,7 @@ dag = DAG(
     default_args=default_args,
     description='Mon premier DAG Airflow',
     schedule_interval=timedelta(days=1),
+    catchup=False
 )
 
 # Tâche d'extraction de données
@@ -40,6 +42,38 @@ def load(**kwargs):
     ti = kwargs['ti']
     transformed_data = ti.xcom_pull(task_ids='transform')
     print("Données chargées :", transformed_data)
+
+def print_params(**kwargs):
+    dag_run = kwargs.get('dag_run')
+    params = dag_run.conf if dag_run else {}
+    user = params.get('user', 'no user provided')
+    print(f"Username received: {user}")
+
+def launch_hello(**kwargs):
+    dag_run = kwargs.get('dag_run')
+    user = None
+    if dag_run and dag_run.conf:
+        user = dag_run.conf.get('user')
+    print("Hello Airflow,", user)
+
+def update_variable(**kwargs):
+    current_user = Variable.get("user", default_var="default_user")
+    new_user = current_user + "_updated"
+    Variable.set("user", new_user)
+    ti = kwargs['ti']
+    ti.xcom_push(key='updated_user', value=new_user)
+
+def say_hello(**kwargs):
+    ti = kwargs['ti']
+    user = ti.xcom_pull(task_ids='update_variable', key='updated_user')
+    print(f"Hello Airflow, {user}!")
+
+hello_task = PythonOperator(
+    task_id='hello_task',
+    python_callable=say_hello,
+    provide_context=True,
+    dag=dag,
+)
 
 # Définir les opérateurs
 extract_task = PythonOperator(
@@ -63,5 +97,12 @@ load_task = PythonOperator(
     dag=dag,
 )
 
+update_variable_task = PythonOperator(
+    task_id='update_variable',
+    python_callable=update_variable,
+    provide_context=True,
+    dag=dag,
+)
+
 # Définir les dépendances
-extract_task >> transform_task >> load_task
+update_variable_task >> hello_task >> extract_task >> transform_task >> load_task
